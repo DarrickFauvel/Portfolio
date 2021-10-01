67107bf9b758349b6d5138e039b90d8e27e2a7ad
Vinit Gupta <vinitman35@gmail.com>
2021-10-01 19:40:40 +0530

    style : make logo respond to screen size

--- src/Components/Logo.tsx
+++ src/Components/Logo.tsx
@@ -5,10 +5,10 @@ function Logo() {
     return (
         <div className="Logo">
             <div className="Logo-text">
-                the
+                <span className="Logo-text-left">the</span>
                 <span className="Logo-pop">V</span>
                 <span className="Logo-margin"></span>
-                initgupta
+                <span className="Logo-text-right">initgupta</span>
             </div>
         </div>
     )
