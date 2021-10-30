7e211c362ac48a44630741a09be46a6486a8a5ec
Vinit Gupta <vinitman35@gmail.com>
2021-10-30 12:34:45 +0530

    add dynamic import in social

--- src/Components/Social.tsx
+++ src/Components/Social.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 function Social({...props}) {
-    // const name:string = props.name;
-    // console.log(name)
-    // const icon =  require(`../Assets/Social/${name}.svg`)
+    const name:string = props.name;
+    console.log(name)
+    const icon =  require(`../Assets/Social/${name}.svg`)
     return (
         <div className="Social">
-           {/* <img className="Social-icon" src={icon.default} alt={name} /> 
-           <div className="Social-line"></div> */}
+           <img className="Social-icon" src={icon.default} alt={name} /> 
+           <div className="Social-line"></div>
         </div>
     )
 }
